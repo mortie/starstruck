938b9145cf4345e167f1d9ee9b03d14c75ae1468
Martin Dørum <martid0311@gmail.com>
2022-02-16 13:32:49 +0100

    change some color stuff

--- src/color.rs
+++ src/color.rs
@@ -3,14 +3,14 @@ use glisp::eval::{ValRef, Scope};
 use std::rc::Rc;
 use std::cell::RefCell;
 
-const BLACK: &'static str = "\x1b[30;0m";
-const RED: &'static str = "\x1b[31;0m";
-const GREEN: &'static str = "\x1b[32;0m";
-const YELLOW: &'static str = "\x1b[33;0m";
-const BLUE: &'static str = "\x1b[34;0m";
-const MAGENTA: &'static str = "\x1b[35;0m";
-const CYAN: &'static str = "\x1b[36;0m";
-const WHITE: &'static str = "\x1b[37;0m";
+const BLACK: &'static str = "\x1b[30m";
+const RED: &'static str = "\x1b[31m";
+const GREEN: &'static str = "\x1b[32m";
+const YELLOW: &'static str = "\x1b[33m";
+const BLUE: &'static str = "\x1b[34m";
+const MAGENTA: &'static str = "\x1b[35m";
+const CYAN: &'static str = "\x1b[36m";
+const WHITE: &'static str = "\x1b[37m";
 const RESET: &'static str = "\x1b[0m";
 
 const BOLD_BLACK: &'static str = "\x1b[30;1m";
@@ -31,23 +31,34 @@ impl ColorCtx {
     fn new(state: Rc<State>) -> Self {
         Self {
             state,
-            stack: vec!(RESET),
+            stack: Vec::new(),
         }
     }
 }
 
 fn push_color(ctx: &Rc<RefCell<ColorCtx>>, col: &'static str) -> Result<ValRef, String> {
     ctx.borrow_mut().stack.push(col);
-    Ok(ValRef::String(Rc::new(format!("{}{}", ctx.borrow().state.shell.escape_start(), col))))
+    let c = ctx.borrow();
+    let escape_start = c.state.shell.escape_start();
+    let escape_end = c.state.shell.escape_end();
+    let s = match c.stack.len() {
+        0 => format!("{}{}{}", escape_start, col, escape_end),
+        _ => format!("{}{}{}{}", escape_start, RESET, col, escape_end),
+    };
+    Ok(ValRef::String(Rc::new(s)))
 }
 
 fn pop_color(ctx: &Rc<RefCell<ColorCtx>>) -> Result<ValRef, String> {
     ctx.borrow_mut().stack.pop();
-    let s = match ctx.borrow_mut().stack.last() {
-        Some(s) => *s,
-        None => return Err("The color stack should never be empty!".to_string()),
+    let c = ctx.borrow();
+    let escape_start = c.state.shell.escape_start();
+    let escape_end = c.state.shell.escape_end();
+    let s = match c.stack.last() {
+        None => format!("{}{}{}", escape_start, RESET, escape_end),
+        Some(last) => format!("{}{}{}{}", escape_start, RESET, last, escape_end),
     };
-    Ok(ValRef::String(Rc::new(format!("{}{}", ctx.borrow().state.shell.escape_end(), s))))
+
+    Ok(ValRef::String(Rc::new(s)))
 }
 
 fn color(ctx: &Rc<RefCell<ColorCtx>>, col: &'static str, args: Vec<ValRef>) -> Result<ValRef, String> {
@@ -72,10 +83,8 @@ pub fn init(scope: &Rc<RefCell<Scope>>, state: &Rc<State>) {
     let ctx = Rc::new(RefCell::new(ColorCtx::new(state.clone())));
 
     macro_rules! put{($name: expr, $color: expr) => {
-        {
-            let c = ctx.clone();
-            scope.borrow_mut().put_func($name, Rc::new(move |a, _| color(&c, $color, a)));
-        }
+        let c = ctx.clone();
+        scope.borrow_mut().put_func($name, Rc::new(move |a, _| color(&c, $color, a)));
     }}
 
     put!("black", BLACK);
